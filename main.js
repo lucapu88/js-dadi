5cc447601dc44ee6b1655a1c57919dcbf4c63369
lucapu88 <caputoluca88@gmail.com>
2019-11-16 17:50:45 +0100

    sistemato, da rivedere la parte "if (numero > 6)......"

--- main.js
+++ main.js
@@ -1,17 +1,28 @@
+//Generare un numero casuale tra 1 e 6.
 var numeroCasuale = Math.floor((Math.random() * 6) + 1);
 console.log(numeroCasuale);
-
+//Attraverso un prompt chiedere all'utente un numero tra 1 e 6.
 var numero = prompt('digita un numero tra 1 e 6');
 console.log(numero);
-
+//Stampare un messaggio per dire all'utente se ha vinto oppure no (il numero più alto vince).
 if (numero > numeroCasuale) {
-  console.log('hai vinto');
-
+  console.log('hai vinto'); //Se il numero digitato è più alto del numero casuale
+  document.getElementById('vinci-perdi').innerHTML = ('BRAVO HAI VINTO!!!');
+  document.getElementById('vinci-img').setAttribute('class','visible');
 }
-else {
-  console.log('hai perso');
+if (numero < numeroCasuale) {
+  console.log('hai perso'); //Se il numero digitato è più basso del numero casuale
+  document.getElementById('vinci-perdi').innerHTML = ('Peccato...HAI PERSO!');
+  document.getElementById('perdi-img').setAttribute('class','visible');
 }
 
-if (numero > 6) {
+if (numero > 6) { //Se l'utente digita un numero più alto del numero 6
   console.log('non barare, il numero deve essere da 1 a 6!');
+  document.getElementById('vinci-perdi').innerHTML = ('Non barare!!! Il numero deve essere da 1 a 6!');
+  document.getElementById('baro-img').setAttribute('class','visible');
+}
+
+if (numero == numeroCasuale) { //se il numero digitato è pari al numero casuale
+  document.getElementById('vinci-perdi').innerHTML = ('PAREGGIO');
+  document.getElementById('pareggia-img').setAttribute('class','visible');
 }
